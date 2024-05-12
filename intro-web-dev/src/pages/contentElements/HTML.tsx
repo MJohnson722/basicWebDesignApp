45a5905f27f09be4691cd6bceb0caf4eb0939cda
Fran Miele <162506996+BlueBoi26@users.noreply.github.com>
2024-05-11 22:46:48 -0400

    HTML start
    Worked on the HTML page

---
+++ intro-web-dev/src/pages/contentElements/HTML.tsx
@@ -0,0 +1,39 @@
+function TitleEl() {
+  return <div>Defines the title</div>;
+}
+
+function HeaderEl() {
+  return <div>Changes Font size</div>;
+}
+
+function ParagraphEl() {
+  return <div>Insets text</div>;
+}
+
+function DivEl() {
+  return <div>Groups elements on a webpage</div>;
+}
+
+function BrEl() {
+  return <div>Inserts in a line break</div>;
+}
+
+function ColorEl() {
+  return <div>Changes the color of the element</div>;
+}
+
+function ButtonEl() {
+  return <div>Inserts an interactable button</div>;
+}
+
+function LinkEl() {
+  return <div>Inserts a hyperlink</div>;
+}
+
+function ImageEl() {
+  return <div>Inserts an image</div>;
+}
+
+function StyleEl() {
+  return <div>Changes the style of specific elements</div>;
+}
--- intro-web-dev/src/pages/contentElements/HtmlPage.js
+++ intro-web-dev/src/pages/contentElements/HtmlPage.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 function HtmlPage() {
+  const htmlElements = [Title, Header, Paragraph, Div, Br, Color, Button, Link, Image, Style]
+  const [alertVisable, setAlertVisability] = useState(false);
   return (
-    <div>HtmlPage</div>
-  )
+    <div>
+    <h1>Html Page</h1>
+    {alertVisable && (<Alert onClose={() => setAlertVisability(false)}>Alert</Alert>
+    )}
+      <alert><li>{htmlElements}</li></alert>
+    </div>
+      )
 }
 
 export default HtmlPage
